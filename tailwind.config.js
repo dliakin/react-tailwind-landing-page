a71261aca1899d6251770cd9adc032542c320759
Dmitry Lyakin <d.lyakin@gmail.com>
2022-08-11 12:26:36 +0300

    Add custom fonts
    Add custom fonts and some custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontSize: {
-        "3xl": [
+        "54-70": [
           "54px",
           {
             lineHeight: "70.2px",
           },
         ],
+      },
+      fontFamily: {
+        sans: ["GolosTextWebRegular", ...defaultTheme.fontFamily.sans],
+        golos: ["GolosTextWebRegular"],
+      },
+      backgroundSize: {
+        "55%": "55%",
+      },
+      backgroundPosition: {
+        "bottom-10": "10rem bottom",
       },
     },
   },
