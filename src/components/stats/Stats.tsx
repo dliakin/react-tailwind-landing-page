be35c6279ce4d4a74c4335bb1eebf061c9490d9f
Dmitry Lyakin <d.lyakin@gmail.com>
2022-08-12 10:29:51 +0300

    Update Stats component. Add items to props

--- src/components/stats/Stats.tsx
+++ src/components/stats/Stats.tsx
@@ -1,28 +1,15 @@
 import React from "react";
+import { IStatItem } from "../../models/models";
 
-function Stats() {
+function Stats({ statItems }: { statItems: IStatItem[] }) {
   return (
-    <div className="container mx-auto grid justify-center grid-cols-2 text-center lg:grid-cols-3">
-      <div className="flex flex-row justify-center items-center">
-        <p className="text-[#ED7138] font-semibold text-7xl ">293</p>
-        <p className="text-[#0F1214] font-normal text-base tracking-[0.005em] text-left ml-4">
-          HD видеоурока <br />
-          длительностью 3 - 15 минут
-        </p>
-      </div>
-      <div className="flex flex-row justify-center items-center">
-        <p className="text-[#ED7138] font-semibold text-7xl ">6</p>
-        <p className="text-[#0F1214] font-normal text-base tracking-[0.005em] text-left ml-4">
-          мероприятий на курсе (Хакатоны,
-          <br /> мастер классы, разборы и т.д.)
-        </p>
-      </div>
-      <div className="flex flex-row justify-center items-center">
-        <p className="text-[#ED7138] font-semibold text-7xl ">193</p>
-        <p className="text-[#0F1214] font-normal text-base tracking-[0.005em] text-left ml-4">
-          заданий и тестов <br /> для закрепления
-        </p>
-      </div>
+    <div className="container mx-auto grid justify-center grid-cols-2 text-center lg:grid-cols-3 mt-[80px]">
+      {statItems.map((item: IStatItem, index: number) => (
+        <div className="flex flex-row justify-center items-center" key={"statItem-" + index}>
+          <p className="text-[#ED7138] font-semibold text-7xl ">{item.statNumber}</p>
+          <p className="text-[#0F1214] font-normal text-base tracking-[0.005em] text-left ml-4">{item.text}</p>
+        </div>
+      ))}
     </div>
   );
 }
