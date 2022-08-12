b2aee7fa1b7c20b9a2b1c7fa96503e45d2803c43
Dmitry Lyakin <d.lyakin@gmail.com>
2022-08-13 00:18:47 +0300

    Fix addoptive view for Card component

--- src/components/cards/CardWithHeader.tsx
+++ src/components/cards/CardWithHeader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function CardWithHeader({ header, children, maxWidth }: { header: string; children: JSX.Element; maxWidth?: string }) {
   return (
-    <div className={`bg-white border border-solid border-[#947EFF] rounded-2xl shadow-cardWithHeader w-max ${maxWidth ? maxWidth : "max-w-prose"}`}>
+    <div className={`bg-white border border-solid border-[#947EFF] rounded-2xl shadow-cardWithHeader 1450px:w-max ${maxWidth ? maxWidth : "max-w-prose"}`}>
       <div className="flex flex-col items-left justify-center my-6 mx-5">
         <h3 className="font-semibold text-sm uppercase text-[#9088FF]">{header}</h3>
         {children}
