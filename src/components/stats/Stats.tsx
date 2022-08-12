07bf0daa324618b130bc82d0439a9d145a8f5aac
Dmitry Lyakin <d.lyakin@gmail.com>
2022-08-13 00:35:00 +0300

    fix css

--- src/components/stats/Stats.tsx
+++ src/components/stats/Stats.tsx
@@ -4,7 +4,7 @@ import StatItem from "./StatItem";
 
 function Stats({ statItems }: { statItems: IStatItem[] }) {
   return (
-    <div className="container mx-auto grid justify-center grid-cols-1 text-center sm:grid-cols-3 mt-[80px]">
+    <div className="container mx-auto grid justify-center grid-cols-1 text-center sm:grid-cols-3 mt-[80px] max-w-6xl xl:px-0 px-4">
       {statItems.map((item: IStatItem, index: number) => (
         <StatItem statValue={item.statValue} text={item.text} key={`stat-item-${index}`} />
       ))}
